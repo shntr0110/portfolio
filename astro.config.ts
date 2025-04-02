cc1c342f6847c04d1a953b55b8d94cd095a425f8
shntr0110 <shintaro.xxx0110@gmail.com>
2025-04-02 16:52:59 +0900

    サイトURL修正

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,11 @@
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [sitemap(), tailwind({ nesting: true }), icon()],
+  site: 'https://tokuyama-shintaro-portfolio.vercel.app/',
+  integrations: [tailwind({ nesting: true }), icon()],
   vite: {
     define: {
       'import.meta.vitest': 'undefined',
